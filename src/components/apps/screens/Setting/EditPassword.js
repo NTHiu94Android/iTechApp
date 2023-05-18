db6245889d58b53d8efe06b5cb92440b8bb33ed4
AnTran0910 <108501507+AnTran0910@users.noreply.github.com>
2023-05-18 13:12:06 +0700

    edit
    cập nhật sửa mật khẩu

--- src/components/apps/screens/Setting/EditPassword.js
+++ src/components/apps/screens/Setting/EditPassword.js
@@ -6,24 +6,53 @@ const EditPassword = () => {
     <View style={styles.container}>
       <View style={styles.header}>
         <Image
-        style={styles.img} 
-        source={require('../../../../assets/images/back.png')} ></Image>
+          style={styles.img}
+          source={require('../../../../assets/images/back.png')} ></Image>
         <Text style={styles.title}>Update password</Text>
       </View>
 
       <View style={styles.body}>
+        <View style={styles.viewInput}>
           <TextInput
-          style={styles.input}
-          placeholder = "Current password"></TextInput>
+            style={styles.input}
+            keyboardType='password'
+            placeholder="Current password"></TextInput>
 
-          <TextInput
-          style={styles.input}
-          placeholder = "New password"></TextInput>
-        
+          <Pressable>
+            <Image
+              style={styles.icon}
+              source={require('../../../../assets/images/blind.png')} >
+            </Image>
+          </Pressable>
+        </View>
 
+        <View style={styles.viewInput}>
           <TextInput
-          style={styles.input}
-          placeholder = "Retype password"></TextInput>
+            style={styles.input}
+            keyboardType='password'
+            placeholder="New password"></TextInput>
+
+          <Pressable>
+            <Image
+              style={styles.icon}
+              source={require('../../../../assets/images/blind.png')} >
+            </Image>
+          </Pressable>
+        </View>
+
+        <View style={styles.viewInput}>
+          <TextInput
+            style={styles.input}
+            keyboardType='password'
+            placeholder="Retype password"></TextInput>
+
+          <Pressable>
+            <Image
+              style={styles.icon}
+              source={require('../../../../assets/images/blind.png')} >
+            </Image>
+          </Pressable>
+        </View>
 
         <Pressable style={styles.btn}>
           <Text style={styles.txtBtn}>Submit</Text>
@@ -40,61 +69,74 @@ export default EditPassword
 const styles = StyleSheet.create({
   footer: {
     flex: 0.5,
-   },
+  },
 
-// body
-txtBtn:{
-  color: 'white',
-  fontSize: 16,
-},
-btn:{
-  marginVertical: 15,
-  justifyContent: 'center',
-  alignItems: 'center',
-  height: 50,
-  width: 150,
-  borderRadius: 10,
-  backgroundColor: 'black',
-},
-input:{
-  paddingHorizontal: 15,
-  height: 40,
-  width: 300,
-  borderBottomWidth: 1,
-  marginVertical: 15,
+  // body
+  txtBtn: {
+    color: 'white',
+    fontSize: 16,
+  },
+  btn: {
+    marginVertical: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 50,
+    width: 150,
+    borderRadius: 10,
+    backgroundColor: 'black',
+  },
+  viewInput: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  icon: {
+    width: 16,
+    height: 16,
+    position: 'absolute',
+    alignItems: 'flex-end',
+  },
+  input: {
+    position: 'relative',
 
-},
-txtTitle:{
+    paddingHorizontal: 15,
+    height: 40,
+    width: 300,
+    borderBottomWidth: 1,
+    marginVertical: 15,
 
-},
+  },
+  txtTitle: {
+
+  },
   body: {
     flex: 5,
     marginVertical: 20,
     alignItems: 'center',
-   },
+  },
 
 
-// header
+  // header
 
-   title:{
-    marginHorizontal : 25,
+  title: {
+    marginHorizontal: 25,
     fontWeight: 'bold',
     color: 'black',
-    fontSize: 18, 
-   },
-   img:{
-    width: 16, 
+    fontSize: 18,
+  },
+  img: {
+    width: 16,
     height: 16,
-   },
-  header: { 
-    marginHorizontal : 15,
+  },
+  header: {
+    marginHorizontal: 15,
     justifyContent: 'flex-start',
     alignItems: 'center',
     flex: 0.5,
     flexDirection: 'row',
 
-   },
-  container: { 
+  },
+  container: {
     flex: 1,
   },
 
