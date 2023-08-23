95b4c30f5dce881308ab18347040355440cfcb1b
NguyenTrongHieu <ngochuongtr1975@gmail.com>
2023-08-23 10:25:55 +0700

    Upd Final

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import messaging from '@react-native-firebase/messaging';
+import { LogBox } from "react-native";
+LogBox.ignoreLogs(["Warning: ", "EventEmitter.removeListener"]); // Ignore log notification by message
+LogBox.ignoreAllLogs(); //Ignore all log notifications
 
 import Started from './src/components/splash/Started';
 
